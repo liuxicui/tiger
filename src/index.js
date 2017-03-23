aec6813c8351b4dbb1b41b1267e472f70fdf39bc
liuxicui <870566031@qq.com>
2017-03-23 11:06:42 +0800

    change

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Routers from "./routers.js"
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './main.css';
+
+injectTapEventPlugin();
+
+render(<Routers />,document.getElementById('root'));
